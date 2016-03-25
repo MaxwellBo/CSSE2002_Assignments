092bb234239f919751c6a4e798c31a44a07ba0a1
Max Bo <max@maxbo.me>
2016-03-25 13:44:30 +1000

    :white_check_mark: test: Add loop test and rename vars

--- Railway/src/railway/test/SectionTest.java
+++ Railway/src/railway/test/SectionTest.java
@@ -14,21 +14,28 @@ import java.util.HashSet;
  */
 public class SectionTest {
 
-    JunctionBranch endPoint1;
-    JunctionBranch endPoint2;
-    JunctionBranch endPoint1a; // Same params as endpoint1
-    JunctionBranch endPoint2a; // To test for non-reference equality
+    JunctionBranch endPoint1F;
+    JunctionBranch endPoint1N; // To test loops
+    JunctionBranch endPoint2R;
+    JunctionBranch endPoint1Fc; // Same params as endPoint1F
+    JunctionBranch endPoint2Rc; // To test for non-reference equality
+    JunctionBranch endPoint3N;
 
     @Before
     public void setUp() {
-         endPoint1 =
+        endPoint1F =
                 new JunctionBranch(new Junction("j1"), Branch.FACING);
-         endPoint2 =
+        endPoint1N =
+                new JunctionBranch(new Junction("j1"), Branch.NORMAL);
+        endPoint2R =
                 new JunctionBranch(new Junction("j2"), Branch.REVERSE);
-        endPoint1a =
+        endPoint1Fc =
                 new JunctionBranch(new Junction("j1"), Branch.FACING);
-        endPoint2a =
+        endPoint2Rc =
                 new JunctionBranch(new Junction("j2"), Branch.REVERSE);
+        endPoint3N =
+                new JunctionBranch(new Junction("j3"), Branch.NORMAL);
+
     }
 
     /** Test Section with valid parameters */
@@ -36,19 +43,19 @@ public class SectionTest {
     public void testBasicSection() {
 
         // setup
-        Section section1 = new Section(5, endPoint1, endPoint2);
+        Section section1 = new Section(5, endPoint1F, endPoint2R);
 
         // check length getter
         Assert.assertEquals(5, section1.getLength());
 
         // check endpoint set getter
         HashSet<JunctionBranch> endPoints12 = new HashSet<>();
-        endPoints12.add(endPoint1);
-        endPoints12.add(endPoint2);
+        endPoints12.add(endPoint1F);
+        endPoints12.add(endPoint2R);
         Assert.assertEquals(endPoints12, section1.getEndPoints());
 
         // check endpoint partner getter
-        Assert.assertEquals(section1.otherEndPoint(endPoint1), endPoint2);
+        Assert.assertEquals(section1.otherEndPoint(endPoint1F), endPoint2R);
 
         // check toString method
         String s1s = section1.toString();
@@ -57,25 +64,37 @@ public class SectionTest {
         Assert.assertTrue(s1s.equals(optionA) || s1s.equals(optionB));
 
         // check equals method
-        Section section2 = new Section(5, endPoint1a, endPoint2a);
+        Section section2 = new Section(5, endPoint1Fc, endPoint2Rc);
         Assert.assertTrue(section1.equals(section2));
 
         // check invariants
         Assert.assertTrue(section1.checkInvariant());
     }
-    // TODO: Test for the things at the top of Section.java
+
+    @Test
+    public void testConstructorLoop() { // Both on same junction
+        Section section1 = new Section(5, endPoint1F, endPoint1N);
+        Assert.assertTrue(section1.checkInvariant());
+    }
+
     @Test(expected = NullPointerException.class)
-    public void testNullConstructor () {
-        Section section1 = new Section(5, null, null);
+    public void testConstructorNull () {
+        Section section1 = new Section(5, endPoint1F, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testNegativeLength() {
-        Section section1 = new Section(-5, endPoint1, endPoint2);
+    public void testConstructorNegativeLength() {
+        Section section1 = new Section(-5, endPoint1F, endPoint2R);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testEquivalentEndpoints() {
-        Section section1 = new Section(1, endPoint1, endPoint1a);
+    public void testConstructorEquivalentEndpoints() {
+        Section section1 = new Section(1, endPoint1F, endPoint1Fc);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetterInvalidEndpointParam() {
+        Section section1 = new Section(5, endPoint1F, endPoint2R);
+        section1.otherEndPoint(endPoint3N);
     }
 }
