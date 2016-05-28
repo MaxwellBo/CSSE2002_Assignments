bac6704d3ce42d05062eb4eba2632f4dcf606d73
Max Bo <max@maxbo.me>
2016-05-28 20:35:00 +1000

    :new: feat: Mop up loadRoute for testing

--- assign3/assignment3/src/railway/gui/RailwayModel.java
+++ assign3/assignment3/src/railway/gui/RailwayModel.java
@@ -8,6 +8,8 @@ import railway.Route;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Predicate;
 
 /**
@@ -15,13 +17,32 @@ import java.util.function.Predicate;
  */
 public class RailwayModel {
 
-    Track track;
+    private Track track;
+    private Map<Integer, Train> trains;
+
+    private class Train {
+
+        final int id;
+        final int startOffset;
+        final int endOffset;
+        final Route subroute;
+        final Route route;
+
+        public Train(int id, int startOffset, int endOffSet, Route route) {
+            this.id = id;
+            this.startOffset = startOffset;
+            this.endOffset = endOffSet;
+            this.subroute = route.getSubroute(startOffset, endOffSet);
+            this.route = route;
+        }
+    }
 
 
     /**
      * Initialises the model for the Railway Manager.
      */
     public RailwayModel() {
+        this.trains = new HashMap<>();
     }
 
     public void loadTrack(String filename) throws IOException, FormatException {
@@ -35,7 +56,7 @@ public class RailwayModel {
         Route route = RouteReader.read(filename);
 
         // (ii)
-        if (!(route.onTrack(track)){
+        if (!(route.onTrack(track))) {
             throw new RuntimeException("The route was loaded,"
                     + " but it is not on the train management system’s track");
         }
@@ -50,9 +71,24 @@ public class RailwayModel {
         }
 
         // (iv)
-       if(false) {
+        // Checks if a train intersects with the captured requested route
+        Predicate<Train> checkIntersectWithRoute = train -> {
+            // Capture here
+            Route requested = route.getSubroute(startOffset, endOffset);
+            return train.subroute.intersects(requested);
+        };
 
-       }
+        if (trains.values()
+                .stream()
+                .anyMatch(checkIntersectWithRoute)) {
+            throw new RuntimeException("Requested subroute intersects with"
+                    + " at least one of the sub-routes currently"
+                    + " allocated to another train");
+        }
+
+        // all checks finished
+        // fallthrough to
+        Train toAdd = new Train(trains.size(), startOffset, endOffset, route);
+        trains.put(trains.size(), toAdd);
     }
-
 }
